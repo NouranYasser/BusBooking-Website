6245d11961941d83eb10c1cc94bfaa0a3f6bb6bd
MariamHossam565 <130882791+MariamHossam565@users.noreply.github.com>
2023-05-08 18:33:55 -0700

    Update dbConnection.js

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'bus_booking',
+    database: 'internet_application',
     port : "3306"
   });
   connection.connect((err)=>{
