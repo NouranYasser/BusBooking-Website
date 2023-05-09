b4ec70e0cb220d0ee0a30b432a75964e60a2d99a
NouranYasser <118839565+NouranYasser@users.noreply.github.com>
2023-05-09 04:36:09 +0300

    Update internet_application.sql

--- internet_application.sql
+++ internet_application.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `internet_application`
+-- Database: `Internet_application`
 --
 
 -- --------------------------------------------------------
